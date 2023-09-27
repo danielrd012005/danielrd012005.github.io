01cd8d3e9e38741e4c3169467ba4527f4fa004d4
danielrd012005 <146118082+danielrd012005@users.noreply.github.com>
2023-09-27 08:48:26 -0600

    Add files via upload

---
+++ proyect (2)/formulario.js
@@ -0,0 +1,69 @@
+// (function(){
+    var formulario = document.getElementById('formularios'),
+    nombre = formulario.nombre,
+    correo = formulario.correo,
+    sexo = formulario.sexo,
+    terminos = formulario.terminos,
+    error = document.getElementById('error');
+
+    function validarNombre(e){
+        if(nombre.value == '' || nombre.value == null){
+            console.log('Por favor completa el nombre');
+            error.style.display = 'block';
+            error.innertHTML += '<li>Por favor completa el nombre</li>';
+            
+            e.preventDefault();
+        } else{
+            error.style.display = 'none';
+        }
+        
+        }
+
+    function validarCorreo(e){
+        if(correo.value == '' || correo.value == null){
+            console.log('Por favor completa el correo');
+            error.style.display = 'block';
+            error.innerHTML += '<li>Por favor completa el correo</li>';
+
+            e.preventDefault();
+        } else {
+            error.style.display = 'none';
+        }
+    }
+
+    function validarSexo(e){
+        if(sexo.value == '' || sexo.value == null){
+            console.log('Por favor completa el sexo');
+            error.style.display = 'block';
+            error.innerHTML += '<li>Por favor completa el sexo</li>';
+
+
+            e.preventDefault();
+        } else {
+            error.style.display = 'none';
+        }
+    }        
+
+    function validarTerminos(e){
+            if(terminos.checked == false){
+                console.log('Por favor acepta los terminos');
+                error.style.display = 'block';
+                error.innerHTML += '<li>Por favor acepta los terminos</li>'; 
+
+                e.preventDefault();
+            } else {
+                error.style.display = 'none';
+            }
+    }
+
+    function validarformulario(e){
+        error.innerHTML = '';
+
+        validarNombre(e);
+        validarCorreo(e);            
+        validarSexo(e);
+        validarTerminos(e);
+    }
+
+    formularios.addEventListener('submit', validarformulario);
+//}())
---
+++ proyect(28)/scrip.js
@@ -0,0 +1,6 @@
+// let|var edad
+
+let nombre = "ammi";
+nombre = 23;
+nombre = 2.2;
+nombre.log(nombre); 
